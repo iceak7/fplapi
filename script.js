3a8e1837187837999d23c4d0a605c0bcdb2e914d
Isak Jensen <isak.jensen@telia.com>
2019-12-05 08:32:21 +0100

    new v

--- script.js
+++ script.js
@@ -9,8 +9,42 @@ app.use(express.static(__dirname+"/public"))
 
 app.get("/",(req,res)=>{
 
-  res.send("Test");
+  res.send("FPL Api Helper");
 
+});
+app.get("/static",(req,res)=>{
+  (async function(){
+  
+    try{
+    let url="https://fantasy.premierleague.com/api/bootstrap-static/";
+    let data = await fetch(url);
+    data = await data.json()
+    res.send(data);
+    }
+    
+    catch(err)
+    {
+    console.log(err);
+    }
+  })();
+
+});
+
+app.get("/transfers/:id", (req,res)=>{
+  (async function(){
+  
+    try{
+    let url="https://fantasy.premierleague.com/api/entry/"+req.params.id+"/transfers/";
+    let data = await fetch(url);
+    data = await data.json();
+    res.send(data);
+    }
+    catch(err)
+    {
+    console.log(err);
+    }
+  })();
+    
 });
 
 app.get("/:id",(req,res)=>{
@@ -35,14 +69,6 @@ console.log(err);
 
 
 })();
-
-
-
-
-
-
-
-
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("http is running on port 3000"));
