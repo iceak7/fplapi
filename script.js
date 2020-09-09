bc8dfdc1bad27a5232a512b2355bd437e7ee5719
unknown <isak.jensen@telia.com>
2020-09-09 16:45:41 +0200

    bug test

--- script.js
+++ script.js
@@ -1,123 +1,109 @@
-const fetch = require('node-fetch'); 
 const cors = require("cors");
+const axios = require("axios");
 const express = require("express");
 
 const app = express();
 app.use(cors());
 
-app.use(express.static(__dirname+"/public"))
+app.use(express.static(__dirname + "/public"));
 
-app.get("/",(req,res)=>{
-
+app.get("/", (req, res) => {
   res.send("FPL Api Helper");
-
 });
-app.get("/static",(req,res)=>{
-  (async function(){
-  
-    try{
-    let url="https://fantasy.premierleague.com/api/bootstrap-static/";
-    let data = await fetch(url);
-    data = await data.json()
-    res.send(data);
-    }
-    
-    catch(err)
-    {
+app.get("/static", async (req, res) => {
+  try {
+    let url = "https://fantasy.premierleague.com/api/bootstrap-static/";
+
+    const resp = await axios.get(url, {
+      mode: "no-cors",
+      headers: { accept: "application/json" },
+    });
+    res.send(resp.data);
+  } catch (err) {
     console.log(err);
-    }
-  })();
-
+  }
 });
 
-app.get("/gwteam/:id/gw/:gw", (req,res)=>{
-  (async function(){
-  
-    try{
-    let url="https://fantasy.premierleague.com/api/entry/"+req.params.id+"/event/"+req.params.gw+"/picks/";
-    let data = await fetch(url);
-    data = await data.json();
-    res.send(data);
-    }
-    catch(err)
-    {
+app.get("/gwteam/:id/gw/:gw", async (req, res) => {
+  try {
+    let url =
+      "https://fantasy.premierleague.com/api/entry/" +
+      req.params.id +
+      "/event/" +
+      req.params.gw +
+      "/picks/";
+
+    const resp = await axios.get(url, {
+      mode: "no-cors",
+      headers: { accept: "application/json" },
+    });
+    res.send(resp.data);
+  } catch (err) {
     console.log(err);
-    }
-  })();
+  }
 });
 
-
-app.get("/transfers/:id", (req,res)=>{
-  (async function(){
-  
-    try{
-    let url="https://fantasy.premierleague.com/api/entry/"+req.params.id+"/transfers/";
-    let data = await fetch(url);
-    data = await data.json();
+app.get("/transfers/:id", async (req, res) => {
+  try {
+    let url =
+      "https://fantasy.premierleague.com/api/entry/" +
+      req.params.id +
+      "/transfers/";
+    const resp = await axios.get(url, {
+      mode: "no-cors",
+      headers: { accept: "application/json" },
+    });
     res.send(data);
-    }
-    catch(err)
-    {
+  } catch (err) {
     console.log(err);
-    }
-  })();
-    
+  }
 });
 
-app.get("/element-summary/:id",(req,res)=>{
-  (async function(){
-  
-    try{
-    let url="https://fantasy.premierleague.com/api/element-summary/"+req.params.id+"/";
-    let data = await fetch(url);
-    data = await data.json();
+app.get("/element-summary/:id", async (req, res) => {
+  try {
+    let url =
+      "https://fantasy.premierleague.com/api/element-summary/" +
+      req.params.id +
+      "/";
+    const resp = await axios.get(url, {
+      mode: "no-cors",
+      headers: { accept: "application/json" },
+    });
     res.send(data);
-    }
-    catch(err)
-    {
+  } catch (err) {
     console.log(err);
-    }
-  })();
+  }
 });
 
-app.get("/history/:id",(req,res)=>{
-  (async function(){
-  
-    try{
-    let url="https://fantasy.premierleague.com/api/entry/"+req.params.id+"/history/";
-    let data = await fetch(url);
-    data = await data.json();
+app.get("/history/:id", async (req, res) => {
+  try {
+    let url =
+      "https://fantasy.premierleague.com/api/entry/" +
+      req.params.id +
+      "/history/";
+    const resp = await axios.get(url, {
+      mode: "no-cors",
+      headers: { accept: "application/json" },
+    });
     res.send(data);
-    }
-    catch(err)
-    {
+  } catch (err) {
     console.log(err);
-    }
-  })();
+  }
 });
 
-app.get("/entry/:id",(req,res)=>{
+app.get("/entry/:id", async (req, res) => {
+  try {
+    let url =
+      "https://fantasy.premierleague.com/api/entry/" + req.params.id + "/";
+    const resp = await axios.get(url, {
+      mode: "no-cors",
+      headers: { accept: "application/json" },
+    });
 
-  console.log(req.params.id);
-
-(async function(){
-  
-try{
-let url="https://fantasy.premierleague.com/api/entry/"+req.params.id+"/";
-let data = await fetch(url);
-data = await data.json()
-
-res.send(data);
-
-
-}
-catch(err)
-{
-console.log(err);
-}
-
-
-})();
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+  }
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("http is running on port 3000"));
